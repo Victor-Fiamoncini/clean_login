5527ad1f5a4a31d85dc471b731033af8efea7a52
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-02-08 01:13:09 -0300

    feat: TestShouldReturn500IfNoHTTPRequestIsProvided & TestShouldReturn500IfHTTPRequestHasNoBody tests added

--- src/presentation/routers/login_router_test.go
+++ src/presentation/routers/login_router_test.go
@@ -1,6 +1,7 @@
 package routers_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,6 +25,12 @@ func NewLoginRouter() *LoginRouter {
 }
 
 func (lr *LoginRouter) Route(hr *httpRequest) *httpResponse {
+	if hr == nil || reflect.ValueOf(hr.Body).IsZero() {
+		return &httpResponse{
+			statusCode: 500,
+		}
+	}
+
 	if hr.Body.Email == "" || hr.Body.Password == "" {
 		return &httpResponse{
 			statusCode: 400,
@@ -67,4 +74,22 @@ func TestShouldReturn400IfNoPasswordIsProvided(t *testing.T) {
 	httpResponse := sut.Route(httpRequest)
 
 	assert.Equal(t, 400, httpResponse.statusCode)
+}
+
+func TestShouldReturn500IfNoHTTPRequestIsProvided(t *testing.T) {
+	sut := NewLoginRouter()
+
+	httpResponse := sut.Route(nil)
+
+	assert.Equal(t, 500, httpResponse.statusCode)
+}
+
+func TestShouldReturn500IfHTTPRequestHasNoBody(t *testing.T) {
+	sut := NewLoginRouter()
+
+	httpRequest := &httpRequest{}
+
+	httpResponse := sut.Route(httpRequest)
+
+	assert.Equal(t, 500, httpResponse.statusCode)
 }
