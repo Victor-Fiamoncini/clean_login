ade6b8e5d98a07d36d4a1ce446dc5282cdc7bbf3
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-02-15 23:05:12 -0300

    refactor: add mongo-driver func wrappers

--- src/infra/repositories/load_user_by_email_repository/load_user_by_email_repository_test.go
+++ src/infra/repositories/load_user_by_email_repository/load_user_by_email_repository_test.go
@@ -1,47 +1,22 @@
 package loaduserbyemailrepository_test
 
 import (
-	"context"
-	"log"
 	"testing"
-	"time"
 
 	luber "github.com/Victor-Fiamoncini/auth_clean_architecture/src/infra/repositories/load_user_by_email_repository"
 	"github.com/stretchr/testify/assert"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func mongoConnection() (*mongo.Client, context.Context, *mongo.Collection) {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	mongoClient, connectErr := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongo"))
-
-	if connectErr != nil {
-		log.Fatal(connectErr)
-	}
-
-	userModel := mongoClient.Database("auth_clean_architecture").Collection("users")
-
-	return mongoClient, ctx, userModel
-}
-
-func makeSut() (luber.ILoadUserByEmailRepository, *mongo.Client, context.Context, *mongo.Collection) {
-	mongoClient, ctx, userModel := mongoConnection()
-
-	loadUserByEmailRepository := luber.NewLoadUserByEmailRepository(userModel)
+func makeSut() luber.ILoadUserByEmailRepository {
+	loadUserByEmailRepository := luber.NewLoadUserByEmailRepository(nil)
 
 	loadUserByEmailRepository.SetEmail("valid_email@mail.com")
 
-	return loadUserByEmailRepository, mongoClient, ctx, userModel
+	return loadUserByEmailRepository
 }
 
 func TestShouldReturnNullAndAnErrorIfNoUserIsFound(t *testing.T) {
-	sut, mongoClient, ctx, userModel := makeSut()
-
-	defer userModel.Drop(ctx)
-	defer mongoClient.Disconnect(ctx)
-	defer ctx.Done()
+	sut := makeSut()
 
 	sut.SetEmail("invalid_email@mail.com")
 
@@ -52,18 +27,14 @@ func TestShouldReturnNullAndAnErrorIfNoUserIsFound(t *testing.T) {
 }
 
 func TestShouldReturnAnUserIfUserIsFound(t *testing.T) {
-	sut, mongoClient, ctx, userModel := makeSut()
+	sut := makeSut()
 
-	defer userModel.Drop(ctx)
-	defer mongoClient.Disconnect(ctx)
-	defer ctx.Done()
-
-	userModel.InsertOne(ctx, bson.D{
-		{
-			Key:   "email",
-			Value: "valid_email@mail.com",
-		},
-	})
+	// userModel.InsertOne(ctx, bson.D{
+	// 	{
+	// 		Key:   "email",
+	// 		Value: "valid_email@mail.com",
+	// 	},
+	// })
 
 	user, err := sut.Load()
 
