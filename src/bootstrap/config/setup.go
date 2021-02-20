46d27b7a411b4ddfaad284809041b50ab727262e
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-02-20 03:56:19 -0300

    feat: add fiber base middlewares

--- src/bootstrap/config/setup.go
+++ src/bootstrap/config/setup.go
@@ -2,12 +2,16 @@ package config
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/helmet/v2"
 )
 
 // SetupApp func
 func SetupApp(app *fiber.App) {
-	app.Use(logger.New())
+	app.Use(helmet.New())
 	app.Use(cors.New())
+	app.Use(compress.New())
+	app.Use(logger.New())
 }
